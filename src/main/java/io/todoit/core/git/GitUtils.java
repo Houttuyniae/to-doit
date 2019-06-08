eddbd7f4af3fdaad8e99030b5bcf44a7a1c47faf
owl <122615922@qq.com>
2019-06-08 17:38:36 +0800

    GITUTILS 修改

--- src/main/java/io/todoit/core/git/GitUtils.java
+++ src/main/java/io/todoit/core/git/GitUtils.java
@@ -1,19 +1,13 @@
 package io.todoit.core.git;
 
-import io.todoit.common.expection.BizException;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.api.AddCommand;
 import org.eclipse.jgit.api.CommitCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.internal.storage.file.FileRepository;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
@@ -42,29 +36,29 @@ public class GitUtils {
      * @PostConstruct 被用来修饰一个非静态的void（）方法。
      */
     @PostConstruct
-    private void init(){
+    private void init() {
         try {
             File file = new File(path);
-            if(!file.exists()){
+            if (!file.exists()) {
                 throw new FileNotFoundException();
             }
             git = Git.open(file);
             auth = new UsernamePasswordCredentialsProvider(username, password);
 
         } catch (IOException e) {
-            log.error("init git error" ,e);
+            log.error("init git error", e);
         }
     }
 
-    public static void push(String message){
+    public void push(String message) {
         try {
-            git.add().addFilepattern("*").call();
+            git.add().addFilepattern("-A").call();
             git.commit().setMessage(message).call();
             git.push().setCredentialsProvider(auth).call();
         } catch (GitAPIException e) {
-            log.error("git remote push error" ,e);
+            log.error("git remote push error", e);
             throw new RuntimeException(e);
         }
     }
-
 }
+
